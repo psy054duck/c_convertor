2692f72504dd97afcf022e744be8236a1d49b95e
conditional-recurrence-solver-team <conditional.solver@gmail.com>
2023-09-03 12:54:38 +0800

    sync

--- test/test.c
+++ test/test.c
@@ -15,33 +15,44 @@ extern void assert(bool);
 #define __VERIFIER_assert(x) assert(x)
 
 int main() {
-    int X, Y;
-    int v, x, y;
-    X = __VERIFIER_nondet_int();
-    Y = __VERIFIER_nondet_int();
-    v = 2 * Y - X;
-    y = 0;
-    x = 0;
+    int a, b, p, q, r, s;
+    int x, y;
+    x = __VERIFIER_nondet_int();
+    y = __VERIFIER_nondet_int();
+    // assume_abort_if_not(x >= 1);
+    // assume_abort_if_not(y >= 1);
+    if (x < 1 || y < 1) return 0;
+
+    a = x;
+    b = y;
+    p = 1;
+    q = 0;
+    r = 0;
+    s = 1;
 
     while (1) {
-	// __VERIFIER_assert(2*Y*x - 2*X*y - X + 2*Y - v == 0);
-        if (!(x <= X))
+        // __VERIFIER_assert(1 == p * s - r * q);
+        // __VERIFIER_assert(a == y * r + x * p);
+        // __VERIFIER_assert(b == x * q + y * s);
+
+        if (!(a != b))
             break;
-        // out[x] = y
 
-        if (v < 0) {
-            v = v + 2 * Y;
+        if (a > b) {
+            a = a - b;
+            p = p - q;
+            r = r - s;
         } else {
-            v = v + 2 * (Y - X);
-            y++;
+            b = b - a;
+            q = q - p;
+            s = s - r;
         }
-        x++;
     }
-	// __VERIFIER_assert(2*Y*x - 2*X*y - X + 2*Y - v == 0);
-    __VERIFIER_assert(2*Y*x - 2*x*y - X + 2*Y - v + 2*y == 0);
-    // 2Yx - 2Xy - X + 2Y - v
-    // 2Y(x + 1) - 2Xy - X + 2Y - v - 2Y
-    // 2Yx + 2Y - 2Xy - X - v
-
+    
+    // __VERIFIER_assert(a - b == 0);    
+    // __VERIFIER_assert(p*x + r*y - a == 0);
+    __VERIFIER_assert(q*r - p*s + 1 == 0);
+    // __VERIFIER_assert(q*x + s*y - b == 0);
     return 0;
 }
+
