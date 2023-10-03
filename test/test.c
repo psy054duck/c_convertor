ce47faea75189d0792e5d6f984cfc9964c298bc8
wcl <wangchlin0@163.com>
2023-10-03 23:44:48 +0800

    sync

--- test/test.c
+++ test/test.c
@@ -1,41 +1,37 @@
-// extern void abort(void);
-// extern void __assert_fail(const char *, const char *, unsigned int, const char *) __attribute__ ((__nothrow__ , __leaf__)) __attribute__ ((__noreturn__));
-// void reach_error() { __assert_fail("0", "count_up_down-1.c", 3, "reach_error"); }
-// 
-// void __VERIFIER_assert(int cond) {
-//   if (!(cond)) {
-//     ERROR: {reach_error();abort();}
-//   }
-//   return;
-// }
 #include <stdbool.h>
-extern unsigned int __VERIFIER_nondet_uint(void);
-extern int __VERIFIER_nondet_int(void);
-extern void assert(bool);
-#define __VERIFIER_assert(x) assert(x)
-#define S 1
-
-int x=0;
-
-void eval(void) 
-{
-  while (1) {
-      x=0;
-      break;
-  }
-  return;
+extern void abort(void);
+extern void __assert_fail(const char *, const char *, unsigned int, const char *) __attribute__ ((__nothrow__ , __leaf__)) __attribute__ ((__noreturn__));
+void reach_error() { __assert_fail("0", "brs2.c", 10, "reach_error"); }
+extern void abort(void);
+void assume_abort_if_not(int cond) {
+  if(!cond) {abort();}
 }
+void __VERIFIER_assert(bool cond) { if(!(cond)) { ERROR: {reach_error();abort();} } }
+extern int __VERIFIER_nondet_int(void);
+void *malloc(unsigned int size);
 
+#define N 10000
+	// int a[N];
+	// int b[N];
+	// int c[N];
+	// int d[N];
+int main()
+{
+	int a[N];
+	for (int i = 0; i != N; i++) {
+		a[i] = i;
+	}
+	for (int i = 0; i != N; i++) {
+		__VERIFIER_assert(a[i] == i);
+	}
+	// N = __VERIFIER_nondet_int();
+	// if(N <= 0) return 1;
+	// assume_abort_if_not(N <= 2147483647/sizeof(int));
 
-int main() {
+	// int i;
+	// long long sum[1];
+	// int *a = malloc(sizeof(int)*N);
+	// int *b = malloc(sizeof(int)*N);
 
-  while(1)
-  {
-    eval();
-    __VERIFIER_assert(x==0);    
-  }
-
-  __VERIFIER_assert(x!=0);
-
-  return 0;
+	return 1;
 }
